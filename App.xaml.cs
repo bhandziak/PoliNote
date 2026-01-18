f29134f6b7ad9292b5b1df1b791fdb89bbbc1b1f
Konrad I. <konrad.iwan@op.pl>
2026-01-18 13:44:23 +0100

    init

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PoliNote
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
