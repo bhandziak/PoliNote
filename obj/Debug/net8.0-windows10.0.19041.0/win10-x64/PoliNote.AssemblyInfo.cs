85f8631bcd5c8efc5dc25d9099e93d2f11eeb02f
Konrad I. <konrad.iwan@op.pl>
2026-01-23 19:26:42 +0100

    c

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/PoliNote.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/PoliNote.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PoliNote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d88e341c69fa0ea4c0e6e97a2d36d34f32115eec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+91e27297921e0ba96630a8c0d301d1246431bc5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PoliNote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PoliNote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
