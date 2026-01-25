d7428f939c0c913d561afe3f9d6f1bd076100eac
Konrad I. <konrad.iwan@op.pl>
2026-01-25 22:10:58 +0100

    Działa!

--- Views/AdminChangeRolePage.xaml.cs
+++ Views/AdminChangeRolePage.xaml.cs
@@ -7,9 +7,18 @@ public partial class AdminChangeRolePage : ContentPage
 {
     private readonly AdminChangeRoleViewModel _vm;
 
-    public Guid UserId
+    private string _userId;
+    public string UserId
     {
-        set => _vm.LoadAsync(value);
+        set
+        {
+            _userId = value;
+
+            if (Guid.TryParse(value, out var guid))
+            {
+                _vm.LoadAsync(guid);
+            }
+        }
     }
 
     public AdminChangeRolePage()
--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/PoliNote.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/PoliNote.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PoliNote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c5431e80503daf8c5215d85208408d9db2d9b674")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2f3c8e2a040f0c8f6728c646c9805d04c8f10fe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PoliNote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PoliNote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
